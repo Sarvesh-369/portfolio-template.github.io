d5e5be89e0b5f6f3a59650949c54a43482c516cf
Sarvesh <sarvesh@Sarveshs-MacBook-Pro-1187.local>
2026-05-17 19:36:42 +0530

    updated

--- components/header.js
+++ components/header.js
@@ -3,39 +3,43 @@ class Header extends HTMLElement {
     super();
   }
 
+  
   connectedCallback() {
+    const basePath = window.location.pathname.includes('/pages/') ? '../' : './';
+    const pagesPath = window.location.pathname.includes('/pages/') ? './' : './pages/';
+    
     this.innerHTML = `
       <header>
         <nav class="navbar navbar-expand-lg navbar-dark sticky-top">
-        <h1><a class="navbar-brand" href="/">Your Name</a></h1>
+        <h1><a class="navbar-brand" href="${basePath}index.html">Your Name</a></h1>
         <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="navbar-collapse collapse" id="navbarCollapse" style="">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item mr-2">
-              <a class="nav-link" href="/">Home</a>
+              <a class="nav-link" href="${basePath}index.html">Home</a>
             </li>
             <li class="nav-item mr-2">
-              <a class="nav-link" href="/pages/education.html">Education</a>
+              <a class="nav-link" href="${pagesPath}education.html">Education</a>
             </li>
             <li class="nav-item mr-2">
-              <a class="nav-link" href="/pages/experience.html">Experience</a>
+              <a class="nav-link" href="${pagesPath}experience.html">Experience</a>
             </li>
             <li class="nav-item mr-2">
-              <a class="nav-link" href="/pages/skills.html">Skills</a>
+              <a class="nav-link" href="${pagesPath}skills.html">Skills</a>
             </li>
             <li class="nav-item mr-2">
-              <a class="nav-link" href="/pages/teaching.html">Teaching</a>
+              <a class="nav-link" href="${pagesPath}teaching.html">Teaching</a>
             </li>
             <li class="nav-item mr-2">
-              <a class="nav-link" href="/pages/projects.html">Projects</a>
+              <a class="nav-link" href="${pagesPath}projects.html">Projects</a>
             </li> 
             <li class="nav-item mr-2">
-              <a class="nav-link" href="/pages/publications.html">Publications</a>
+              <a class="nav-link" href="${pagesPath}publications.html">Publications</a>
             </li>
             <li class="nav-item mr-2">
-              <a class="nav-link" href="/pages/blogposts.html">Blogs</a>
+              <a class="nav-link" href="${pagesPath}blogposts.html">Blogs</a>
             </li>
           </ul>
         </div>
